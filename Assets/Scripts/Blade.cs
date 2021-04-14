34cdfa92d351c93743cf0e88bcd89e4f510baf5a
frilel <crillefrille@gmail.com>
2021-04-14 22:50:51 +0200

    Add TimingCircles, InputManager put into Blade.cs
    use everything in Prefabs/Dependencies in your scene to get all dependent functionality

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
 	[SerializeField] GameObject bladeTrailPrefab;
-	[SerializeField] float minCuttingVelocity = .0001f;
+	[SerializeField] float minMouseCuttingVelocity = .0001f;
+	[SerializeField] float minTouchCuttingVelocity = .01f;
 
 	bool isCutting = false;
 
 	Vector2 previousPosition;
+	Vector2 startingTouch;
 
 	GameObject currentBladeTrail;
-
 	Rigidbody2D rb;
 	Camera cam;
 	CircleCollider2D circleCollider;
@@ -26,33 +25,106 @@ public class Blade : MonoBehaviour
 
 	void Update()
 	{
-		if (isCutting)
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+
+		// Use mouse input in editor or computer
+		MouseUpdateCut();
+
+		if (Input.GetMouseButtonDown(0))
 		{
-			UpdateCut();
+			MouseStartCutting();
 		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			MouseStopCutting();
+		}
+#else
+		// Use touch input on mobile
+		if (Input.touchCount == 1)
+		{
+			TouchUpdateCut(Input.GetTouch(0).position);
+
+			// Input check is AFTER TouchUpdateCut, that way if TouchPhase.Ended happened a single frame after the Began Phase
+			// a swipe can still be registered (otherwise, isCutting will be set to false and the test wouldn't happen for that began-Ended pair)
+			if (Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				TouchStartCutting();
+			}
+			else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+			{
+				TouchStopCutting();
+			}
+		}
+#endif
+
 	}
 
-	void UpdateCut()
+	void TouchUpdateCut(Vector2 touchPos)
 	{
-		Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 newPosition = cam.ScreenToWorldPoint(touchPos);
 		rb.position = newPosition;
-		print("newPos: " + newPosition + " - mousePos: " + Input.mousePosition);
 
-		float velocity = (newPosition - previousPosition).magnitude * Time.deltaTime;
+		if (isCutting)
+		{
+			Vector2 diff = touchPos - startingTouch;
 
-		if (velocity > minCuttingVelocity)
-		{
-			circleCollider.enabled = true;
-		}
-		else
-		{
-			circleCollider.enabled = false;
+			// Put difference in Screen ratio, but using only width, so the ratio is the same on both
+			// axes (otherwise we would have to swipe more vertically...)
+			diff = new Vector2(diff.x / Screen.width, diff.y / Screen.width);
+
+			if (diff.magnitude > minTouchCuttingVelocity) //trigger cutting if moving fast enough
+				circleCollider.enabled = true;
+			else
+				circleCollider.enabled = false;
 		}
 
 		previousPosition = newPosition;
+
 	}
 
-	public void StartCutting()
+	public void TouchStartCutting()
+	{
+		isCutting = true;
+		startingTouch = Input.GetTouch(0).position;
+
+		Vector2 newPosition = cam.ScreenToWorldPoint(Input.GetTouch(0).position);
+		this.gameObject.transform.position = newPosition;
+		previousPosition = newPosition;
+
+		currentBladeTrail = Instantiate(bladeTrailPrefab, this.transform);
+	}
+
+	public void TouchStopCutting()
+	{
+		isCutting = false;
+		circleCollider.enabled = false;
+
+		currentBladeTrail.transform.SetParent(null);
+		Destroy(currentBladeTrail, 2f);
+	}
+
+    #region MouseInput
+    void MouseUpdateCut()
+	{
+		Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		rb.position = newPosition;
+
+		if (isCutting)
+		{
+			float velocity = (newPosition - previousPosition).magnitude * Time.deltaTime;
+
+			if (velocity > minMouseCuttingVelocity)
+				circleCollider.enabled = true;
+			else
+				circleCollider.enabled = false;
+		}
+
+		previousPosition = newPosition;
+
+	}
+
+	public void MouseStartCutting()
 	{
 		isCutting = true;
 
@@ -63,7 +135,7 @@ public class Blade : MonoBehaviour
 		currentBladeTrail = Instantiate(bladeTrailPrefab, this.transform);
 	}
 
-	public void StopCutting()
+	public void MouseStopCutting()
 	{
 		isCutting = false;
 		circleCollider.enabled = false;
@@ -71,4 +143,5 @@ public class Blade : MonoBehaviour
 		currentBladeTrail.transform.SetParent(null);
 		Destroy(currentBladeTrail, 2f);
 	}
+    #endregion MouseInput
 }
