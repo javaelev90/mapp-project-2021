6e9f7a60e229e3a1baf5af4a96e174584020653e
alicewallin <71320509+alicewallin@users.noreply.github.com>
2021-04-28 17:02:55 +0200

    save volume in database

--- Assets/VolumeMenu.cs
+++ Assets/VolumeMenu.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeMenu : MonoBehaviour
 {
+
     public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider effectSlider;
+
+    private void Start()
+    {
+        float musicVolume = Database.Instance.settingsRepository.GetVolumeSetting();
+        audioMixer.SetFloat("musicVolume", musicVolume);
+        musicSlider.value = musicVolume;
+
+    }
 
     public void SetMusicVolume(float volume)
     {
         audioMixer.SetFloat("musicVolume", volume);
+        Database.Instance.settingsRepository.UpdateVolumeSetting(volume);
         
     }
 
     public void SetEffectsVolume(float volume)
     {
         audioMixer.SetFloat("effectsVolume", volume);
+
     }
 }
