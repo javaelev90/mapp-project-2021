230c4749acfbb1b67c3abd2d8e562dbecc44a57a
alicewallin <71320509+alicewallin@users.noreply.github.com>
2021-05-24 13:35:45 +0200

    Send leaderboard

--- Assets/Scripts/PlayFabManager.cs
+++ Assets/Scripts/PlayFabManager.cs
@@ -50,6 +50,24 @@ public class PlayFabManager : MonoBehaviour
     }
     void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result) {
         Debug.Log("Successful leaderboard sent");
+        //send leaderboard i gamecontroller
+
+    }
+    public void GetLeaderboard() {
+        var request = new GetLeaderboardRequest
+        {
+            StatisticName = "PlatformScore",
+            StartPosition = 0,
+            MaxResultsCount = 10
+        };
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
+    }
+    void OnLeaderboardGet(GetLeaderboardResult result) {
+        Debug.Log(result.ToString());
+        foreach (var item in result.Leaderboard) {
+            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+
+        }
 
     }
 }
